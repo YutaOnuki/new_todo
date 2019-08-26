18523ffa032e300a8f64a0ed6a26bb936a77fe65
Yuta Onuki <onu4648yu@gmail.com>
2019-08-26 17:34:42 +0900

    layout:About、Contactページ作成

---
+++ frontend/src/components/contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- 内容はお好みで -->
+    <p>If you want to contact me, you send mail to below address.</p>
+    <p>test@example.com</p>
+    <li><router-link to="/">Top</router-link></li>
+    <li><router-link to="/about">About</router-link></li>
+    <li><router-link to="/contact">Contact</router-link></li>
+  </div>
+</template>
